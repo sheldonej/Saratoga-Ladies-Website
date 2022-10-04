3f7d149407bbcf2ea878c0ab8f00aed4ff713400
Adrian Rodriguez @ RPI '24 <arodr517@gmail.com>
2022-10-04 17:09:12 -0400

    Update 404.js

--- gatsby-wp/src/pages/404.js
+++ gatsby-wp/src/pages/404.js
@@ -2,7 +2,7 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-
+//hello world
 const NotFoundPage = () => (
   <Layout>
     <h1>404: Not Found</h1>
