618ab35e46055da911ad967b1a7aa7a63306417c
Adrian Rodriguez @ RPI '24 <arodr517@gmail.com>
2022-10-16 23:04:32 -0400

    Quadrant page found at ./rodria11

---
+++ gatsby-wp/src/components/adrianq/adrianqcomps.js
@@ -0,0 +1,47 @@
+import * as React from 'react'
+
+export function Q1() {
+    return (
+            <div style={{height: "400px", width: "50%", backgroundColor: "blue"}}>
+                <div style={{ 
+                    height: "200px", 
+                    width: "200px", 
+                    backgroundColor: "black", 
+                    margin: "100px", 
+                    transform: "rotate(45deg)"}}/>
+            </div>
+            
+    );
+}
+
+export function Q2() {
+    return (
+        <div style={{height: "400px", width: "50%", backgroundColor: "green"}}>
+            <div style={{
+                height: "200px",
+                width: "200px",
+                backgroundColor: "red",
+                borderRadius: "50%",
+                margin: "100px"
+            }}/>
+        </div>
+            
+    );
+}
+
+export function Q3() {
+    return (
+        <div style={{height: "400px", width: "50%", backgroundColor: "pink"}}>       
+            <h1>Q3</h1>
+        </div> 
+    );
+}
+
+export function Q4() {
+    return (
+        <div style={{height: "400px", width: "50%", backgroundColor: "orange"}}>
+            <h1>Q4</h1>
+        </div>
+        
+    );
+}
