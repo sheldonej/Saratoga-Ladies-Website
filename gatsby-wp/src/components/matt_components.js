9e8ff14f90d3fbba8b7ba02d3b232ca789a37e03
Matthew Merritt <merrittlmatt@gmail.com>
2022-10-24 19:35:40 -0400

    added component for plan quadrant 1 goal.

---
+++ gatsby-wp/src/components/matt_components.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import "../components/matt_shapes.css"
+
+export function Q1() {
+    return (
+        <div className="quadrant1">
+            <div className="diamond"> </div>
+        </div>
+    );
+}
