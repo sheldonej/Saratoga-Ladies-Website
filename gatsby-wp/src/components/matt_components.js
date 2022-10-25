4c90f623865b4d6137e3f8052f1219774573cb21
Matthew Merritt <merrittlmatt@gmail.com>
2022-10-24 20:32:53 -0400

    Added centering on quad 1 element.
    added 3 components, creating 4 total components for matthew's page.

--- gatsby-wp/src/components/matt_components.js
+++ gatsby-wp/src/components/matt_components.js
@@ -1,10 +1,36 @@
 import * as React from 'react'
 import "../components/matt_shapes.css"
 
+
+
 export function Q1() {
     return (
-        <div className="quadrant1">
+        <div className="quadrant1 parentStyle" >
             <div className="diamond"> </div>
+        </div>
+    );
+}
+
+export function Q2() {
+    return (
+        <div className="quadrant2 parentStyle" >
+            <div className="circle"> </div>
+        </div>
+    );
+}
+
+export function Q3() {
+    return (
+        <div className="quadrant3 parentStyle" >
+            <div className="textbox"> Some text </div>
+        </div>
+    );
+}
+
+export function Q4() {
+    return (
+        <div className="quadrant4 parentStyle" >
+            This text is centered over 2 lines.
         </div>
     );
 }
--- gatsby-wp/src/pages/merrim5.js
+++ gatsby-wp/src/pages/merrim5.js
@@ -3,10 +3,16 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import "../components/matt_shapes.css"
+import { Q1, Q2, Q3, Q4} from "../components/matt_components"
 
 const MatthewsPage = () => (
   <Layout>
     <h1 style={{ textAlign: "center"}}>Matthew Merritt</h1>
+    <Q1/>
+    <Q2/>
+    <Q3/>
+    <Q4/>
     <p style={{ textAlign: "center"}}>This is basic text to test on a page.</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
