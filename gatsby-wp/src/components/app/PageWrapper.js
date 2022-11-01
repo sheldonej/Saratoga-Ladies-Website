37dd1d2de1930c799a94e6c316836642c4e9d8c3
Adrian Rodriguez @ RPI '24 <arodr517@gmail.com>
2022-11-01 17:24:46 -0400

    Update PageWrapper.js

--- gatsby-wp/src/components/app/PageWrapper.js
+++ gatsby-wp/src/components/app/PageWrapper.js
@@ -6,49 +6,7 @@ import { Link, navigate } from "gatsby"
 import { useState } from 'react'
 import HeaderContent from "./HeaderContent"
 
-const HeaderData = [
-  {
-    title: "TITLE A",
-    path: "a",
-    icon: "a",
-    cName: "a"
-  },
-
-  {
-    title: "TITLE B",
-    path: "b",
-    icon: "b",
-    cName: "b"
-  },
-
-  {
-    title: "TITLE C",
-    path: "c",
-    icon: "c",
-    cName: "c"
-  },
-
-  {
-    title: "TITLE D",
-    path: "d",
-    icon: "d",
-    cName: "d"
-  }
-
-];
-
 function Header(props) {
-  const [display, setDisplay] = useState( 'none' )
-
-  function handleClick() {
-    if (display == 'none'){
-      setDisplay( 'block' )
-    }
-    else {
-      setDisplay( 'none' )
-    }
-  }
-  
   return (
     <header>
       <HeaderContent/>
