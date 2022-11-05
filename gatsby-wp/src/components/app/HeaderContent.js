2a428b0b0316177a816b2c3cbced3b5d43f8b498
Adrian Rodriguez @ RPI '24 <arodr517@gmail.com>
2022-11-05 04:56:34 -0400

    logo always centered now ! + update dropdown

--- gatsby-wp/src/components/app/HeaderContent.js
+++ gatsby-wp/src/components/app/HeaderContent.js
@@ -12,7 +12,7 @@ function HeaderContent(){
 
     function handleDropDownClick() {
         if (display == 'none'){
-            setDisplay( 'block' )
+            setDisplay( 'flex' )
         }
         else {
             setDisplay( 'none' )
@@ -22,24 +22,26 @@ function HeaderContent(){
     return (
         <div className="header-wrapper">
             <div className="container1">
-            <Link to="/rodria11">Adrian's Webpage</Link>
+                <Link to="/rodria11">Adrian's Webpage</Link>
             </div>
             <div className="container2">
-            <Link to="/hadav">Vishal's Webpage</Link>
+                <Link to="/hadav">Vishal's Webpage</Link>
             </div>
             <div className="imageContainer" onClick={()=>navigate('/')}>
                 <img src={IMG} />
             </div>
             <div className="container3">
-            <Link to="/merrim5">Matthew's Webpage</Link>
+                <Link to="/merrim5">Matthew's Webpage</Link>
             </div>
             <div className="dropcontainer" onClick={()=>handleDropDownClick()}>
-            DROP CONTAINER
-            {HeaderData.map((item) => {
-                return ( <div className="dropdown-content" style={{display: display}}>
-                {item.title}
-                </div> )
-            })}
+                DROP CONTAINER
+                <div className="drop-content-container" style={{display: display}}>
+                    {HeaderData.map((item) => {
+                        return ( <div className="dropdown-content">
+                        {item.title}
+                        </div> )
+                    })}
+                </div>
             </div>
             <svg viewBox="0 0 40 2" fill="#fff"><path d={`M0 0 Q 20 2.75, 40 0`} /></svg>
         </div>
