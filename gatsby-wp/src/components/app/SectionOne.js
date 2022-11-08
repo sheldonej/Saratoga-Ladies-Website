56d61abf952207f1054fb10da01bffe913edf0ea
vhada7 <hadav@rpi.edu>
2022-11-08 18:08:34 -0500

    LinedHeader Component

--- gatsby-wp/src/components/app/SectionOne.js
+++ gatsby-wp/src/components/app/SectionOne.js
@@ -1,15 +1,12 @@
 import * as React from "react"
 import "./SectionOne.css"
+import LinedHeader from "./LinedHeader"
 
 const SectionOne = () => {
     return (
 <div className = "section-header">
     {/* Make this a component */}
-    <div className = "flex vertical-center " >
-        <h2 className = "section-header-lines">
-            <div className="section-header-text">Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet </div>
-        </h2>
-    </div>
+    <LinedHeader/>
 
     <div className = "flex vertical-center ">
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
