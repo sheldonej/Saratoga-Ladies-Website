a450ec858248d1669aa9b2d87384e50d87808cb7
vhada7 <hadav@rpi.edu>
2022-11-18 16:02:03 -0500

    renaming images

---
+++ gatsby-wp/src/components/HomePage/SectionOne/SectionElements.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+
+export const ContentContainer = styled.div.attrs({
+    className: 'section-header',
+})`
+width:100%;
+padding: 10px;
+height: fit-content;
+margin: 0px auto 20px auto;
+`
