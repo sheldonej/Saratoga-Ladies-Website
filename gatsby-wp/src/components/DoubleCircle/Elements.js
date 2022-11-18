a1f897589470b06a40b990596c8501757545d5dd
Matthew Merritt <merrittlmatt@gmail.com>
2022-11-18 16:49:36 -0500

    Cleaned-up double circle elements
    *Changed used css components to styled components javascript convention for react

--- gatsby-wp/src/components/DoubleCircle/Elements.js
+++ gatsby-wp/src/components/DoubleCircle/Elements.js
@@ -1,2 +1,34 @@
 // css to this file for style components 
 //See Section 3 for guide
+
+import styled from 'styled-components'
+
+export const ContentContainer = styled.div`
+    height: auto;
+    width: -webkit-fill-available;
+    padding: 4em 4em;
+    gap: 50px;
+
+    display: flex;
+    flex-flow: ${props => props.reverse ? 'row-reverse' : 'row' };
+`
+
+export const CirclesContainer = styled.div`
+    height: auto;
+    width: -webkit-fill-available;
+    position: relative;
+
+    display: flex;
+    flex-flow: ${props => props.reverse ? 'row-reverse' : 'row' };
+`
+
+
+
+export const InnerCircleContainer = styled.div`
+    position: absolute;
+    transform: translateX(-50%);
+    bottom: -10px;
+
+    display: flex;
+    flex-flow: ${props => props.reverse ? 'row-reverse' : 'row' };
+`
--- gatsby-wp/src/components/DoubleCircle/index.jsx
+++ gatsby-wp/src/components/DoubleCircle/index.jsx
@@ -1,15 +1,12 @@
 // import * as React from 'react'
 import React, { useState } from 'react';
 import "./matt_shapes.css"
+import { CirclesContainer, ContentContainer, InnerCircleContainer } from './Elements'
 import outerCircleImage from "../../images/cemetery/IMG8.jpg"
 import innerCircleImage from "../../images/cemetery/IMG15.jpg"
 
 
-// components to be used for page component 4 (in development)
-// change name of the component to double circle
-// (TODO): use usestate(for functional components - set up logic based off of "s" "m" "l" for circles) 
-// that would adjust the size of the circles (from props input)
-// (TODO): add the new double circle into a larger component that uses "flex row (css) for styling"
+// TODO: Add documentation
 
 // make image height equals 100
 function DoubleCircles(props) {
@@ -18,43 +15,20 @@ function DoubleCircles(props) {
     const [circleInnerSize, setCircleInnerSize] = useState("m");
 
   return (
-    <div id="double-circles">
+        <ContentContainer>
+            <CirclesContainer>
+                <div id={"circle-" + circleOuterSize} className="overflow-hidden">
+                    <img src={outerCircleImage} className="circle-image" alt="outer circle" />
 
-        {/* outer div*/}
-        <div className="parent">
-            {/* outer circle */}
-            <div id={"circle-" + circleOuterSize} className="overflow-hidden">
-                <img src={outerCircleImage} className="circle-image" alt="outer circle" />
-                {/* inner div */}
-                <div className="child">
-                    {/* inner circle */}
-                    <div id="circle-m" className="overflow-hidden">
-                        <img src={innerCircleImage} className="circle-image" alt="inner circle" />
+                    <div className="child">
+                        <div id={"circle-" + circleInnerSize} className="overflow-hidden">
+                            <img src={innerCircleImage} className="circle-image" alt="inner circle" />
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
+            </CirclesContainer>
+        </ContentContainer>
   );
-}
-
-// convert above to style css 
-
-
-export function SectionOne(props) {
-    return (
-        <div className="flex-direction" >
-            {/* Add a text and circles elements */}
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                nisi ut aliquip ex ea commodo consequat.
-
-            </p>
-            <DoubleCircles {...props} />
-        </div>
-    )
 }
 
 // (TODO): move utilized component to element.js, and utilize js version of css
