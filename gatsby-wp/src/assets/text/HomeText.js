cde905773732a5867bb6c60ff79684ec44e0c707
Matthew Merritt <merrittlmatt@gmail.com>
2022-12-05 20:24:49 -0500

    Centralized all text on landing page to single file
    *The constants exported will provide an easy way for all the text on the website to be centralized.
    *This allows for easy updates to the site, without needing to search
    *- the codebase for where the text-to-be-updated is located

--- gatsby-wp/src/assets/text/HomeText.js
+++ gatsby-wp/src/assets/text/HomeText.js
@@ -1,22 +1,60 @@
 /*----------------------------------------------------------------*/
 /*          Text for the hero section of the landing page         */
 /*----------------------------------------------------------------*/
-export const HeroRibbonText = "Lorem ipsum dolor sit amet"
+export const HeaderRibbonText = "Lorem ipsum dolor sit amet"
 
 /*----------------------------------------------------------------*/
-/*            Text for section one of the landing page            */
+/*                  Section 1 of Landing Page                     */
 /*----------------------------------------------------------------*/
-export const SectionOneHeader = "Excepteur Sint Occaecat Cupidatat"
-export const SectionOneParagraphs = "Enim neque volutpat ac tincidunt vitae semper. Nulla aliquet enim tortor at auctor urna. Porttitor leo a diam sollicitudin tempor. Adipiscing enim eu turpis egestas pretium aenean. Sed blandit libero volutpat sed cras ornare arcu dui vivamus. Aliquam etiam erat velit scelerisque in dictum. Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque viverra. Nec ultrices dui sapien eget mi proin sed libero enim. Turpis egestas maecenas pharetra convallis posuere morbi leo. Varius sit amet mattis vulputate enim nulla aliquet porttitor lacus."
+export const SectionOneHeader = "LOREM IPSUM DOLOR SIT AMET"
+export const SectionOneParagraph1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sapien pellentesque habitant morbi tristique senectus et. Mollis aliquam ut porttitor leo a diam sollicitudin. Ipsum a arcu cursus vitae congue mauris rhoncus aenean. Morbi quis commodo odio aenean sed adipiscing diam. Et leo duis ut diam quam. Non nisi est sit amet. Mi eget mauris pharetra et ultrices neque ornare aenean euismod. Scelerisque felis imperdiet proin fermentum leo. Morbi tristique senectus et netus et. Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Ac odio tempor orci dapibus ultrices in iaculis nunc sed. Hac habitasse platea dictumst quisque sagittis purus sit. Nunc vel risus commodo viverra maecenas. Sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum. Hac habitasse platea dictumst vestibulum. Nibh praesent tristique magna sit amet purus gravida."
+export const SectionOneParagraph2 = "Integer feugiat scelerisque varius morbi enim nunc. Nibh tortor id aliquet lectus proin. Ipsum dolor sit amet consectetur adipiscing elit ut. In hac habitasse platea dictumst quisque. Erat nam at lectus urna duis convallis. Turpis egestas sed tempus urna et pharetra. A diam sollicitudin tempor id. Tempus imperdiet nulla malesuada pellentesque. Praesent semper feugiat nibh sed pulvinar. Massa sapien faucibus et molestie ac feugiat. Neque laoreet suspendisse interdum consectetur libero id. Odio ut enim blandit volutpat maecenas volutpat blandit aliquam. Enim neque volutpat ac tincidunt vitae semper quis lectus nulla. Tellus in metus vulputate eu scelerisque felis imperdiet proin. Cursus vitae congue mauris rhoncus aenean. Egestas maecenas pharetra convallis posuere morbi leo. Habitasse platea dictumst quisque sagittis purus sit amet volutpat consequat. Eget nulla facilisi etiam dignissim diam quis."
+export const SectionButton = "Learn More About The Saratoga Ladies"
 
 /*----------------------------------------------------------------*/
-/*            Text for section two of the landing page            */
+/*                  Section 2 of Landing Page                     */
 /*----------------------------------------------------------------*/
-export const SectionTwoHeader = "Excepteur Sint Occaecat Cupidatat"
-export const SectionTwoParagraphs = "Enim neque volutpat ac tincidunt vitae semper. Nulla aliquet enim tortor at auctor urna. Porttitor leo a diam sollicitudin tempor. Adipiscing enim eu turpis egestas pretium aenean. Sed blandit libero volutpat sed cras ornare arcu dui vivamus. Aliquam etiam erat velit scelerisque in dictum. Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque viverra. Nec ultrices dui sapien eget mi proin sed libero enim. Turpis egestas maecenas pharetra convallis posuere morbi leo. Varius sit amet mattis vulputate enim nulla aliquet porttitor lacus."
+export const SectionTwoHeader = "LOREM IPSUM"
+export const SectionTwoParagraph1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+export const SectionTwoParagraph2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
 
 /*----------------------------------------------------------------*/
-/*            Text for section three of the landing page          */
+/*                  Section 3 of Landing Page                     */
 /*----------------------------------------------------------------*/
-export const SectionThreeHeader = "Excepteur Sint Occaecat Cupidatat"
-export const SectionThreeParagraphs = "Enim neque volutpat ac tincidunt vitae semper. Nulla aliquet enim tortor at auctor urna. Porttitor leo a diam sollicitudin tempor. Adipiscing enim eu turpis egestas pretium aenean. Sed blandit libero volutpat sed cras ornare arcu dui vivamus. Aliquam etiam erat velit scelerisque in dictum. Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque viverra. Nec ultrices dui sapien eget mi proin sed libero enim. Turpis egestas maecenas pharetra convallis posuere morbi leo. Varius sit amet mattis vulputate enim nulla aliquet porttitor lacus."
+export const SectionThreeHeader = "Lorem ipsum dolor sit amet"
+export const SectionThreeSubHeader = "Excepteur Sint Occaecat Cupidatat"
+export const SectionThreeParagraph1 = "Enim neque volutpat ac tincidunt vitae semper. Nulla aliquet enim tortor at auctor urna. Porttitor leo a diam sollicitudin tempor. Adipiscing enim eu turpis egestas pretium aenean. Sed blandit libero volutpat sed cras ornare arcu dui vivamus. Aliquam etiam erat velit scelerisque in dictum. Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque viverra. Nec ultrices dui sapien eget mi proin sed libero enim. Turpis egestas maecenas pharetra convallis posuere morbi leo. Varius sit amet mattis vulputate enim nulla aliquet porttitor lacus."
+
+/*----------------------------------------------------------------*/
+/*                      Section 1 of Footer                       */
+/*----------------------------------------------------------------*/
+export const FooterSectionOneHeader = "Our Mission"
+export const FooterSectionOneParagraph = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas."
+
+
+/*----------------------------------------------------------------*/
+/*                      Section 2 of Footer                       */
+/*----------------------------------------------------------------*/
+export const FooterSectionTwoHeader = "Join Our Email List"
+export const FooterSectionTwoParagraph = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas."
+export const FooterSectionTwoButton = "Sign Up"
+
+/*----------------------------------------------------------------*/
+/*                      Section 3 of Footer                       */
+/*----------------------------------------------------------------*/
+export const FooterSectionThreeHeader = "Navigation"
+export const FooterSectionTwoItem1 = "Jose"
+export const FooterSectionTwoItem2 = "Blog"
+export const FooterSectionTwoItem3 = "Donations"
+export const FooterSectionTwoItem4 = "FAQ"
+export const FooterSectionTwoItem5 = "New"
+export const FooterSectionTwoItem6 = "Set"
+export const FooterSectionTwoItem7 = "of"
+export const FooterSectionTwoItem8 = "links"
+
+/*----------------------------------------------------------------*/
+/*                      Section 4 of Footer                       */
+/*----------------------------------------------------------------*/
+export const FooterSectionFourItem1 = "Lorem Ipsum"
+export const FooterSectionFourItem2 = "Dalor Amut"
+export const FooterSectionFourItem3 = "© 2022 Saratoga Ladies"
