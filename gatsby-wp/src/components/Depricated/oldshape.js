5da09745e8ab7fbc9fcb2ef2e06a1911cdb28ce1
FrodoLFX <51185713+FrodoLFX@users.noreply.github.com>
2022-12-09 21:03:55 -0500

    Add files via upload

---
+++ gatsby-wp/src/components/Depricated/oldshape.js
@@ -0,0 +1,40 @@
+import * as React from "react"
+import "./Shapes.css"
+
+export const Square = ({background, text}) => {
+    return (
+        <div style = { {backgroundColor:background}} class="component1">
+            <div id ="square"></div>
+        </div>
+    );
+}
+
+export const Square2nd = ({background, text}) => {
+    return (
+        <div style = { {backgroundColor:background}} class="component1">
+            <div id ="square2nd"></div>
+        </div>
+    );
+}
+
+export const TextAr = ({background, text}) => {
+    return (
+        <div style = { {backgroundColor:background}} class="component3">
+            <textarea id ="textar"></textarea>
+        </div>
+    );
+}
+export const CenteredTextAr = ({background, text}) => {
+    return (
+        <div style = { {backgroundColor:background}} class="component4">
+            <p id ="centeredtextar">{text}</p>
+        </div>
+    );
+}
+export const Circle = ({background, text}) => {
+    return (
+        <div style = { {backgroundColor:background}} class="component2">
+            <div class="circles">{text}</div>
+        </div>
+    );
+}
